b854a41f985f9d4865b82d13a116d0473f25bdee
ForsythIshan <ishand1005@gmail.com>
2025-07-02 21:16:12 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "@/lib/agGridSetup";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased bg-gray-50 min-h-screen`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
